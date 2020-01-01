89f0778e665eb70bba3aefd5aeef466678fa6c1e
Alex Z <Aleks-Z@users.noreply.github.com>
2020-01-02 00:21:19 +0300

    fixed by me for stam with l0ve
    dll, not an console exe dude!

--- src/EpicMorg.SteamPathsLib/SteamPathsUtil.cs
+++ src/EpicMorg.SteamPathsLib/SteamPathsUtil.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SteamPathsLib {
-    class Program {
-        static void Main(string[] args) {
-        }
+namespace EpicMorg.SteamPathsLib
+{
+    public class SteamPathsUtil
+    {
     }
 }
