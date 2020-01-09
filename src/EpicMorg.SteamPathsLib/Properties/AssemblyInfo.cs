83836f2a6b56567d6bd4527c31c127610e10a003
stam <stam@epicm.org>
2020-01-09 16:14:10 +0300

    bump to 1.1.0.0 version in assembly info
    Леха, переводи студию на ENG.

--- src/EpicMorg.SteamPathsLib/Properties/AssemblyInfo.cs
+++ src/EpicMorg.SteamPathsLib/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанные со сборкой.
 [assembly: AssemblyTitle("EpicMorg.SteamPathsLib")]
 [assembly: AssemblyDescription("Steam Paths Library")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Valve")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("e34ae985-7c7e-49ba-a2b0-70d88f865265")]
 
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию 
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
