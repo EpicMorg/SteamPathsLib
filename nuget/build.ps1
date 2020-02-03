0c6f5646920ff37be1924e8c69c27926c3ca8134
STAM <stam@epicm.org>
2020-02-04 00:46:02 +0300

    1.1.0.2

--- nuget/build.ps1
+++ nuget/build.ps1
@@ -1,12 +1,6 @@
 Remove-Item './lib/net48/dummy_file' -Force
-Remove-Item './lib/net472/dummy_file' -Force
-Remove-Item './lib/net462/dummy_file' -Force
-Remove-Item './lib/net452/dummy_file' -Force
 Remove-Item './*.nupkg' -Force
 
 & nuget.exe pack ./EpicMorg.SteamPathsLib.nuspec
 
 Copy-Item './dummy_file' -Destination './lib/net48/dummy_file' -Force
-Copy-Item './dummy_file' -Destination './lib/net472/dummy_file' -Force
-Copy-Item './dummy_file' -Destination './lib/net462/dummy_file' -Force
-Copy-Item './dummy_file' -Destination './lib/net452/dummy_file' -Force
